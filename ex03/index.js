1903ac7e1ba8cbd5024d1c63cd9d77a69ef380fd
geekQiaQia <xiyangbai@outlook.com>
2020-08-25 11:48:58 +0800

    udpate

---
+++ ex03/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs')
+module.exports.parser = path => {
+    const readStream = fs.createReadStream(path)
+    let reqData = [];
+    let size = 0;
+    return new Promise((resolve,reject) => {
+		
+         readStream.on('data',chunk=>{
+			 reqData.push(chunk);
+			 size+=chunk.length;
+		 });
+		 
+		 readStream.on('end',()=>{
+			 const result=Buffer.concat(reqData,size);
+			 let jsonData= JSON.parse(result.toString());
+			 resolve(jsonData)
+		 });
+		 
+		 readStream.on('error',(err)=>{
+			 reject(err);
+		 });
+    })
+}
